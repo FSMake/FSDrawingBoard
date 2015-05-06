8a9020f9c511b9aa2bc12136401ed9dc87296a5b
FSMake <fs_0527@163.com>
2015-05-06 23:10:05 +0800

    Initial Commit

---
+++ 绘图板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  绘图板
+//
+//  Created by 樊樊帅 on 5/6/15.
+//  Copyright (c) 2015 樊樊帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 绘图板/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  绘图板
+//
+//  Created by 樊樊帅 on 5/6/15.
+//  Copyright (c) 2015 樊樊帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
